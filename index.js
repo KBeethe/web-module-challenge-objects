1b9a3d7469235183676bcb828e148a3e4b51c907
KBeethe <67873290+KBeethe@users.noreply.github.com>
2020-07-30 22:00:42 -0600

    Completed MVP.

--- index.js
+++ index.js
@@ -93,11 +93,12 @@ and should return a string in the format `name} gave the restaurant a {rating}, 
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
-function getLastReview(/* code here */) {
-    /* code here */
+function getLastReview(reviews) {
+  let reviewer = reviews[reviews.length -1];
+  return `${reviewer.name} gave the restaurant a ${reviewer.rating} and their feedback was: ${reviewer.feedback}`
   } 
 
-
+getLastReview();
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
 
 /** STRETCH 1: Write a function called `getReviewByRating` that returns an array containing all reviews in a certain range. Your function should accept: 
